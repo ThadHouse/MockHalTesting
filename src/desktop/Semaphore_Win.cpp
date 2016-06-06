9b93ed8113de2f475e4e392c431706f0e2c2b313
Thad House <thadhouse1@gmail.com>
2016-06-05 22:52:52 -0700

    Enables Arm HAL Builds

---
+++ src/desktop/Semaphore_Win.cpp
@@ -0,0 +1,59 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) FIRST 2016. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#ifdef _WIN32
+
+#include "HAL/Semaphore.h"
+
+#include "Log.h"
+
+// set the logging level
+TLogLevel semaphoreLogLevel = logDEBUG;
+
+#define SEMAPHORE_LOG(level)     \
+  if (level > semaphoreLogLevel) \
+    ;                            \
+  else                           \
+  Log().Get(level)
+
+extern "C" {
+
+MUTEX_ID initializeMutexNormal() { return new priority_mutex; }
+
+void deleteMutex(MUTEX_ID sem) { delete sem; }
+
+/**
+ * Lock the mutex, blocking until it's available.
+ */
+void takeMutex(MUTEX_ID mutex) { mutex->lock(); }
+
+/**
+ * Attempt to lock the mutex.
+ * @return true if succeeded in locking the mutex, false otherwise.
+ */
+bool tryTakeMutex(MUTEX_ID mutex) { return mutex->try_lock(); }
+
+/**
+ * Unlock the mutex.
+ * @return 0 for success, -1 for error. If -1, the error will be in errno.
+ */
+void giveMutex(MUTEX_ID mutex) { mutex->unlock(); }
+
+MULTIWAIT_ID initializeMultiWait() { return CreateSemaphore(NULL, 1, 1, NULL); }
+
+void deleteMultiWait(MULTIWAIT_ID cond) { CloseHandle(cond); }
+
+void takeMultiWait(MULTIWAIT_ID cond, MUTEX_ID m) {
+  WaitForSingleObject(cond, INFINITE);
+}
+
+void giveMultiWait(MULTIWAIT_ID cond) { 
+  ReleaseSemaphore(cond, 1, NULL);
+}
+
+}  // extern "C"
+#endif // _WIN32
